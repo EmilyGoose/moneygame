7d6612a89545f1c66072d8a6a187e0c69f2f34a2
Emily Goose <EmilyGoose@users.noreply.github.com>
2023-10-14 11:37:46 -0400

    Burgers have horizontal velocity

--- Assets/Scripts/BurgerFactoryScript.cs
+++ Assets/Scripts/BurgerFactoryScript.cs
@@ -1,12 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BurgerFactoryScript : MonoBehaviour
 {
     // Burger spawn amount
     public int burgersToSpawn = 0;
+    // Burger spawn interval
+    public float spawnInterval = 0F;
     // Prefab of burger
     public GameObject burgerPrefab;
     
@@ -17,7 +21,7 @@ public class BurgerFactoryScript : MonoBehaviour
     void Start()
     {
         borgerList = new List<GameObject>();
-        InvokeRepeating(nameof(spawnBurger), 1f, 1f);
+        InvokeRepeating(nameof(spawnBurger), 1f, spawnInterval);
     }
 
     // Update is called once per frame
@@ -37,8 +41,25 @@ public class BurgerFactoryScript : MonoBehaviour
     {
         if (borgerList.Count < burgersToSpawn)
         {
-            // Todo position of burger spawner
-            borgerList.Add(Instantiate(burgerPrefab, new Vector3(0, 0, 0), Quaternion.identity));
+            // Create new burger
+            GameObject newBurger = Instantiate(burgerPrefab, gameObject.transform.position, Quaternion.identity);
+            // Make it spawn with a bit of outwards force so it doesn't just drop
+            newBurger.GetComponent<Rigidbody>().AddForce(randomVelocityVector());
+            // Keep track of it in the list
+            borgerList.Add(newBurger);
         }
+    }
+
+    Vector3 randomVelocityVector()
+    {
+        // Get random angle rotated around vertical axis
+        Quaternion velocityQuackOnion = Quaternion.AngleAxis(Random.Range(0F, 360F), Vector3.up);
+        // Get Vector3 representing the angle
+        Vector3 velocityVector = velocityQuackOnion * Vector3.forward;
+        // Multiply to make the effect stronger since we start with a unit vector
+        int forceMultiplier =  (int)Random.Range(0, 50F);
+        velocityVector.Scale(new Vector3(forceMultiplier, forceMultiplier, forceMultiplier));
+        
+        return velocityVector;
     }
 }
