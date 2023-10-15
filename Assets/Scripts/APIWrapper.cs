d6bb64a88ed8fbd56e9784f6ecc9b6a1ec23c3a1
Emily Goose <EmilyGoose@users.noreply.github.com>
2023-10-15 01:17:52 -0400

    Almost ready for demo

--- Assets/Scripts/APIWrapper.cs
+++ Assets/Scripts/APIWrapper.cs
@@ -24,8 +24,14 @@ public class APIWrapper : MonoBehaviour
     
     public void RequestAnswer(int guess, string category)
     {
+        // harcode a little bit here to avoid it elsewhere :D
+        var tx_type = "tx_type";
+        // if (category.Equals("Tims"))
+        // {
+        //    tx_type = "company";
+        //}
         jsonBody = "{\"user_id\":\"user_1\",\"expected_amount\":" + guess +
-                          ",\"filter\":{\"tx_type\":\""+ category +"\"}}";
+                          ",\"filter\":{\"" + tx_type + "\":\""+ category +"\"}}";
         
         print(jsonBody);
 
--- Assets/Scripts/GameControllerScript.cs
+++ Assets/Scripts/GameControllerScript.cs
@@ -8,27 +8,28 @@ public class GameControllerScript : MonoBehaviour
 {
     public GameObject textArea;
     public GameObject tacoButton;
-    
+
     // Dictation
     public DictationController dictationController;
-    
+
     // API wrapper
     private APIWrapper backend;
-    
+
     // Jars for comparison
     public GameObject guessJar;
     public GameObject actualJar;
-    
+
     // Borger
     public GameObject burgerFactory;
-    
+
     private TextMeshProUGUI textBox;
     private TacoTrigger tacoTrigger;
 
     private BurgerFactoryScript guessCoinScript;
     private BurgerFactoryScript actualCoinScript;
     private BurgerFactoryScript burgerSpawner;
-    
+    public BurgerFactoryScript coffeeSpawner;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -40,60 +41,99 @@ public class GameControllerScript : MonoBehaviour
         burgerSpawner = burgerFactory.GetComponent<BurgerFactoryScript>();
 
         backend = GetComponent<APIWrapper>();
-        
+
         StartCoroutine(gameCoroutine());
     }
-    
+
     IEnumerator gameCoroutine()
     {
-        
-        textBox.SetText("Welcome to Show Me The Money! Hit the taco to start.");
-
-        // Wait for the trigger volume to be hit
-        while (!tacoTrigger.hit)
+        // Kinda hacky way to run it twice without duplicate code
+        // Ideally I should be storing this text better but this is my middle of the night hack
+        // I will look back at this and cry
+        foreach (
+            var currentStringSet in
+            new[]
+            {
+                new[]
+                {
+                    "Welcome to Show Me The Money! Hit the taco to start.",
+                    "First question! How much did you spend on Food this month?",
+                    "Grocery" // todo actual
+                },
+                new[]
+                {
+                    "When you're ready for the next question, hit the taco!",
+                    "Next question! How much did you spend at Tim's?",
+                    "Fuel" // todo actual
+                }
+            })
         {
-            yield return new WaitForSeconds(0.5F);
-        }
-        
-        textBox.SetText("First question! How much did you spend on food this month?");
-        
-        dictationController.toggleRecord();
-        
-        yield return new WaitForSeconds(6);
-        
-        dictationController.toggleRecord();
-        
-        textBox.SetText("Processing audio...");
+            tacoTrigger.hit = false;
+            textBox.SetText(currentStringSet[0]);
 
-        while (!dictationController.doneProcessing)
-        {
-            yield return new WaitForSeconds(0.5F);
-        }
+            // Wait for the trigger volume to be hit
+            while (!tacoTrigger.hit)
+            {
+                yield return new WaitForSeconds(0.5F);
+            }
 
-        int guessedMoney = parseAnswer(dictationController.resultText);
-        dictationController.resultText = "";
-        
-        textBox.SetText("Contacting server...");
-        
-        backend.RequestAnswer(guessedMoney, "Food");
-        
-        while (!backend.requestComplete)
-        {
-            yield return new WaitForSeconds(0.5F);
-        }
-        backend.requestComplete = false;
-        
-        // Output result
-        textBox.SetText(backend.geePeeTee);
+            // Clear anything left in spawners
+            guessCoinScript.burgersToSpawn = 0;
+            actualCoinScript.burgersToSpawn = 0;
+            burgerSpawner.burgersToSpawn = 0;
+            coffeeSpawner.burgersToSpawn = 0;
 
-        // Spawn coins
-        guessCoinScript.burgersToSpawn = guessedMoney;
-        actualCoinScript.burgersToSpawn = backend.actualValue;
-        
-        // Spawn burgers (around $5 per burger fuck it)
-        burgerSpawner.burgersToSpawn = backend.actualValue / 5;
+            textBox.SetText(currentStringSet[1]);
+
+            // Record dictation for 6s
+            dictationController.toggleRecord();
+            yield return new WaitForSeconds(6);
+            dictationController.toggleRecord();
+
+            textBox.SetText("Processing audio...");
+
+            // Wait for dictation to process
+            while (!dictationController.doneProcessing)
+            {
+                yield return new WaitForSeconds(0.5F);
+            }
+
+            // Grab the first int in the user's answer and hit the API
+            int guessedMoney = parseAnswer(dictationController.resultText);
+            dictationController.resultText = "";
+
+            textBox.SetText("Contacting server...");
+
+            backend.RequestAnswer(guessedMoney, currentStringSet[2]);
+
+            while (!backend.requestComplete)
+            {
+                yield return new WaitForSeconds(0.5F);
+            }
+
+            backend.requestComplete = false;
+
+            // Output result
+            textBox.SetText(backend.geePeeTee);
+
+            // Spawn coins
+            guessCoinScript.burgersToSpawn = guessedMoney;
+            actualCoinScript.burgersToSpawn = backend.actualValue;
+
+            // Spawn burgers (or coffee) (around $5 per burger and $2 per coffee)
+            if (currentStringSet[2].Equals("Grocery"))
+            {
+                burgerSpawner.burgersToSpawn = backend.actualValue / 5;
+            }
+            else
+            {
+                coffeeSpawner.burgersToSpawn = backend.actualValue / 2;
+            }
+            
+            yield return new WaitForSeconds(15F);
+        }
     }
-    
+
     // i love regex
     private int parseAnswer(String matchText)
     {
@@ -112,4 +152,3 @@ public class GameControllerScript : MonoBehaviour
         }
     }
 }
-
